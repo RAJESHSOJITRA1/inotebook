5079248ddc426b1c6c0294f1edc75d5c7b635585
RAJESHSOJITRA1 <rajesh@hcase.in>
2024-08-22 17:25:00 +0530

    authtokengerated for data

--- iNotebookBackend/routes/auth.js
+++ iNotebookBackend/routes/auth.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
-const bcrypt = require('bcrypt');
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+const JWT_SECRET="rajesh sojitra 1";
 
 // create a user using :POST "api/auth/createuser"    No login require
 router.post(
@@ -29,20 +31,28 @@ router.post(
           .json({ error: "sorry the user is already exist" });
       }
       // hash code generated
-      const salt=await  bcrypt.genSalt(10);
-      const  secPass=await bcrypt.hash(req.body.password,salt)
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
       //   create a new user
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password:secPass,
+        password: secPass,
       });
-      res.json({ user });     //response that enter data by user  
+      const data={
+        user:{
+          id:user.id
+        }
+      }
+     const authtoken=   jwt.sign(data,JWT_SECRET)
+     res.json({ authtoken }); 
+
+      res.json({ user }); //response that enter data by user
     } catch (error) {
       console.error(error.message);
       res.status(500).send("Some error is occured");
     }
-}
+  }
 );
 module.exports = router;
 
